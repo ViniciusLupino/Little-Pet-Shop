3e964275267ef0119e8ac597c570da1ef5e17240
Jean <127881305+hamanameia@users.noreply.github.com>
2023-05-09 16:10:14 -0300

    cadastro atualizado

--- js/script.js
+++ js/script.js
@@ -1,15 +1,10 @@
-function abrirNovaPagina() {
-    window.open("Home.html");
-}
+
 
 function pegarEmail() {
 
     let erro = document.getElementById('erro');
     let email = document.getElementById("barraEmail").value;
     let senha = document.getElementById("barraSenha").value;
-
-    let emailAdm = "admin@teste.com.br"
-    let senhaAdm = "!@#ABC123def"
 
     let padraoEmail = /^[\w-\.]+@(gmail\.com|sesisp\.org\.br|hotmail\.com|[a-zA-Z0-9-]+\.[a-zA-Z]{2,3}\.br)$/
     let emailValido = padraoEmail.test(email);
@@ -19,59 +14,59 @@ function pegarEmail() {
 
 
 
-
     erro.style.backgroundColor = '';
     erro.style.color = ''
     erro.style.display = '';
 
 
-    if (emailValido == true) {
-
-        if (senha.trim() == '') {
-            erro.style.display = 'block';
-            erro.innerHTML = ("O campo Senha não pode estar em branco");
-            return false;
-        }
-
-        if (senha.length < 8) {
-            erro.style.display = 'block';
-            erro.innerHTML = ("A Senha deve conter pelo menos 8 caracteres");
-            return false;
-        } else {
-
-            if (email == emailAdm && senha == senhaAdm) {
-                erro.style.display = 'block';
-                erro.style.backgroundColor = 'greenyellow';
-                // erro.style.borderColor = "rgb(140, 204, 44)"
-
-                erro.innerHTML = ("BEM VINDO ADMINISTRADOR");
-
-                let tempoEspera = 2000;
-                setTimeout(abrirNovaPagina, tempoEspera);
-
-                return false;
-
-            } else {
-                // window.location.replace("Home.html");
-                window.open("Home.html");
-            }
-        }
-
-    } else {
+    if (senha.trim() == '') {
         erro.style.display = 'block';
-        erro.innerHTML = ("O Email não é valido");
+        erro.innerHTML = ("O campo Senha não pode estar em branco");
         return false;
     }
+
+    if (senha.length < 8) {
+        erro.style.display = 'block';
+        erro.innerHTML = ("A Senha deve conter pelo menos 8 caracteres");
+        return false;
+    }
+
+    else {
+
+        if (emailValido) {
+            window.location.replace("Home.html");
+        }
+
+        else {
+            erro.style.display = 'block';
+            erro.innerHTML = ("O Email é Invalido");
+            return false;
+        }
+
+    }
+    // --------------------------------------------------------------------------------
+
+
+
+    // --------------------------------------------------------------------------------
+
+
+    let padraoSenha = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{8,}$/;
+
+    let SenhaIsValid = padraoSenha.test(senha);
+
+    if (!SenhaIsValid) {
+        console.log('Campo SENHA não corresponde ao padrão desejado!');
+    }
+
 
 }
 
 
-
+const password = document.getElementById('barraSenha');
+const icon = document.getElementById('icon');
 
 function showHide() {
-    let password = document.getElementById('barraSenha');
-    let icon = document.getElementById('icon');
-
     if (password.type === 'password') {
         password.setAttribute('type', 'text');
         icon.classList.add('hide')
@@ -82,8 +77,1004 @@ function showHide() {
     }
 }
 
-function sumirErro() {
-    let tempoEspera = 2000;
-    setTimeout("erro.style.display = ''", tempoEspera);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function fazerid(){
+    let id = document.getElementById('id');
+    id.innerHTML = parseInt(Math.random()*1000000000);
 }
 
+function validardados(){
+    let erro = document.getElementById('erro1');
+    let nome = frm1.nome.value;
+    let ende = frm1.ende.value;
+    let cid = frm1.cid.value;
+    let uf = frm1.uf.value;
+
+    if (nome.trim() == ''){
+        erro.innerHTML = "o campo nome não pode estar vazio!";
+        erro.style.display = 'block';
+        return false;
+    }
+    if (nome.length < 3){
+        erro.innerHTML = "o campo nome não pode ter menos de 3 caracteres!";
+        erro.style.display = 'block';
+        return false;
+    }
+    if (nome.length > 100){
+        erro.innerHTML = "o campo nome não pode ter mais de 100 caracteres!";
+        erro.style.display = 'block';
+        return false;
+    }
+    // 
+    if (ende.trim() == ''){
+        erro.innerHTML = "o campo endereço não pode estar vazio!";
+        erro.style.display = 'block';
+        return false;
+    }
+    if (ende.length < 3){
+        erro.innerHTML = "o campo endereço não pode ter menos de 3 caracteres!";
+        erro.style.display = 'block';
+        return false;
+    }
+    if (ende.length > 100){
+        erro.innerHTML = "o campo endereço não pode ter mais de 100 caracteres!";
+        erro.style.display = 'block';
+        return false;
+    }
+    // 
+    if (cid.trim() == ''){
+        erro.innerHTML = "o campo cidade não pode estar vazio!";
+        erro.style.display = 'block';
+        return false;
+    }
+    if (cid.length < 3){
+        erro.innerHTML = "o campo cidade não pode ter menos de 3 caracteres!";
+        erro.style.display = 'block';
+        return false;
+    }
+    if (cid.length > 50){
+        erro.innerHTML = "o campo cidade não pode ter mais de 50 caracteres!";
+        erro.style.display = 'block';
+        return false;
+    }
+    // 
+    else{
+        erro.style.display = 'none';
+    }
+    // 
+    if (uf.length >= 2){
+        let padrao = /^[A-Z]{2}$/
+        if (padrao.test(uf) == false){
+            erro.innerHTML = "Coloque em maiuscula a unidade federativa!";
+            erro.style.display = 'block';
+            return false;
+        }
+        else{
+            erro.style.display = 'none';
+        }
+    }
+    else{
+        erro.innerHTML = "Coloque uma unidade federativa válida!";
+        erro.style.display = 'block';
+        return false;
+    }
+};
