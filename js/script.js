c8f430f240c1e581650a742712f6d7e1aae07e2e
Eric Ferreiras Brolio <114818635+Yukkie-bdb@users.noreply.github.com>
2023-05-10 16:00:56 -0300

    Agendamento

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-
+function abrirNovaPagina() {
+    window.location.replace("Home.html");
+}
 
 function pegarEmail() {
 
     let erro = document.getElementById('erro');
     let email = document.getElementById("barraEmail").value;
     let senha = document.getElementById("barraSenha").value;
+
+    let emailAdm = "admin@teste.com.br"
+    let senhaAdm = "!@#ABC123def"
 
     let padraoEmail = /^[\w-\.]+@(gmail\.com|sesisp\.org\.br|hotmail\.com|[a-zA-Z0-9-]+\.[a-zA-Z]{2,3}\.br)$/
     let emailValido = padraoEmail.test(email);
@@ -14,59 +19,58 @@ function pegarEmail() {
 
 
 
+
     erro.style.backgroundColor = '';
     erro.style.color = ''
     erro.style.display = '';
 
 
-    if (senha.trim() == '') {
-        erro.style.display = 'block';
-        erro.innerHTML = ("O campo Senha não pode estar em branco");
-        return false;
-    }
+    if (emailValido == true) {
 
-    if (senha.length < 8) {
-        erro.style.display = 'block';
-        erro.innerHTML = ("A Senha deve conter pelo menos 8 caracteres");
-        return false;
-    }
-
-    else {
-
-        if (emailValido) {
-            window.location.replace("Home.html");
-        }
-
-        else {
+        if (senha.trim() == '') {
             erro.style.display = 'block';
-            erro.innerHTML = ("O Email é Invalido");
+            erro.innerHTML = ("O campo Senha não pode estar em branco");
             return false;
         }
 
+        if (senha.length < 8) {
+            erro.style.display = 'block';
+            erro.innerHTML = ("A Senha deve conter pelo menos 8 caracteres");
+            return false;
+        } else {
+
+            if (email == emailAdm && senha == senhaAdm) {
+                erro.style.display = 'block';
+                erro.style.backgroundColor = 'greenyellow';
+
+                erro.innerHTML = ("BEM VINDO ADMINISTRADOR");
+
+                let tempoEspera = 2000;
+                setTimeout(abrirNovaPagina, tempoEspera);
+
+                return false;
+
+            } else {
+                // window.location.replace("Home.html");
+                window.location.replace("Home.html");
+            }
+        }
+
+    } else {
+        erro.style.display = 'block';
+        erro.innerHTML = ("O Email não é valido");
+        return false;
     }
-    // --------------------------------------------------------------------------------
-
-
-
-    // --------------------------------------------------------------------------------
-
-
-    let padraoSenha = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{8,}$/;
-
-    let SenhaIsValid = padraoSenha.test(senha);
-
-    if (!SenhaIsValid) {
-        console.log('Campo SENHA não corresponde ao padrão desejado!');
-    }
-
 
 }
 
 
-const password = document.getElementById('barraSenha');
-const icon = document.getElementById('icon');
+
 
 function showHide() {
+    let password = document.getElementById('barraSenha');
+    let icon = document.getElementById('icon');
+
     if (password.type === 'password') {
         password.setAttribute('type', 'text');
         icon.classList.add('hide')
@@ -75,5 +79,10 @@ function showHide() {
         password.setAttribute('type', 'password');
         icon.classList.remove('hide')
     }
+}
+
+function sumirErro() {
+    let tempoEspera = 2000;
+    setTimeout("erro.style.display = ''", tempoEspera);
 }
 
