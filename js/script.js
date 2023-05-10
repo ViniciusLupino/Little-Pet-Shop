3fae1fa7c33d10c667373da2e0908547f9d8cf4e
Vinícius de Mattos Lupino <viniciusdemattoslupino@gmail.com>
2023-05-10 08:43:14 -0300

    Dashboard vs 1.0.0.2

--- js/script.js
+++ js/script.js
@@ -77,19 +77,26 @@ function showHide() {
     }
 }
 
-// *****************************************************
+// --------------------------------------------------------------------------------
 
-var aleatorio = Math.random() * 10;
+// Gráfico 1
+
+var aleatorio = Math.random() * 20;
 var valoresCertos = Math.round(aleatorio);
-var aleatorio1 = Math.random() * 10;
+
+var aleatorio1 = Math.random() * 20;
 var valoresCertos1 = Math.round(aleatorio1);
-var aleatorio2 = Math.random() * 10;
+
+var aleatorio2 = Math.random() * 20;
 var valoresCertos2 = Math.round(aleatorio2);
-var aleatorio3 = Math.random() * 10;
+
+var aleatorio3 = Math.random() * 20;
 var valoresCertos3 = Math.round(aleatorio3);
-var aleatorio4 = Math.random() * 10;
+
+var aleatorio4 = Math.random() * 20;
 var valoresCertos4 = Math.round(aleatorio4);
-var aleatorio5 = Math.random() * 10;
+
+var aleatorio5 = Math.random() * 20;
 var valoresCertos5 = Math.round(aleatorio5);
 
 
@@ -97,7 +104,7 @@ google.charts.load("current", { packages: ['corechart'] });
 google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
-        ["Element", "Density", { role: "style" }],
+        ["Element", "Atendimento diário", { role: "style" }],
         ["Segunda-feira", valoresCertos1, "#00769f"],
         ["Terça-feira", valoresCertos2, "#069ac5"],
         ["Quarta-feira", valoresCertos3, "#0388b2"],
@@ -122,9 +129,79 @@ function drawChart() {
         bar: { groupWidth: "90%" },
         legend: { position: "none" },
         backgroundColor: "#4AF0B6",
-        vAxis: {gridlines:{color: 'black'}},
+        vAxis: { gridlines: { color: 'black' } },
     };
     var chart = new google.visualization.ColumnChart(document.getElementById("Grafico1"));
+    chart.draw(view, options);
+}
+
+// --------------------------------------------------------------------------------
+
+// Gráfico 2
+
+var aleatorioCachorros1 = Math.random() * 20;
+var valoresCertosCachorros1 = Math.round(aleatorioCachorros1);
+
+var aleatorioCachorros2 = Math.random() * 20;
+var valoresCertosCachorros2 = Math.round(aleatorioCachorros2);
+
+var aleatorioCachorros3 = Math.random() * 20;
+var valoresCertosCachorros3 = Math.round(aleatorioCachorros3);
+
+var aleatorioCachorros4 = Math.random() * 20;
+var valoresCertosCachorros4 = Math.round(aleatorioCachorros4);
+
+var aleatorioCachorros5 = Math.random() * 20;
+var valoresCertosCachorros5 = Math.round(aleatorioCachorros5);
+
+var aleatorioCachorros6 = Math.random() * 20;
+var valoresCertosCachorros6 = Math.round(aleatorioCachorros6);
+
+var aleatorioCachorros7 = Math.random() * 20;
+var valoresCertosCachorros7 = Math.round(aleatorioCachorros7);
+
+var aleatorioCachorros8 = Math.random() * 20;
+var valoresCertosCachorros8 = Math.round(aleatorioCachorros8);
+
+var aleatorioCachorros9 = Math.random() * 20;
+var valoresCertosCachorros9 = Math.round(aleatorioCachorros9);
+
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart2);
+function drawChart2() {
+    var data = google.visualization.arrayToDataTable([
+        ["Element", "Principais raças de cachorros que cuidamos", { role: "style" }],
+        ["Spitz Alemão Anão - Pomerânia", valoresCertosCachorros1, "#004c90"],
+        ["Bulldog Francês", valoresCertosCachorros2, "#116db5"],
+        ["Rottweiler", valoresCertosCachorros3, "#095da3"],
+        ["Pug", valoresCertosCachorros4, "#1a7ec8"],
+        ["Golden Retriever", valoresCertosCachorros5, "#228eda"],
+        ["Pastor Alemão", valoresCertosCachorros6, "#004c90"],
+        ["Yorkshire Terrier", valoresCertosCachorros7, "#116db5"],
+        ["Border Collie", valoresCertosCachorros8, "#095da3"],
+        ["Sem raça definida", valoresCertosCachorros9, "#1a7ec8"],
+    ]);
+
+    var view = new google.visualization.DataView(data);
+    view.setColumns([0, 1,
+        {
+            calc: "stringify",
+            sourceColumn: 1,
+            type: "string",
+            role: "annotation"
+        },
+        2]);
+
+    var options = {
+        title: "Quais as principais raças de cachorros que nós tratamos, em média, em um mês",
+        width: 600,
+        height: 400,
+        bar: { groupWidth: "90%" },
+        legend: { position: "none" },
+        backgroundColor: "#4AF0B6",
+        hAxis: { gridlines: { color: 'black' } },
+    };
+    var chart = new google.visualization.BarChart(document.getElementById("Grafico2"));
     chart.draw(view, options);
 }
 
