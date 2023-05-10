b56cacb14e7c38af55f11dbbb38cb2ac774b5306
Vinícius de Mattos Lupino <viniciusdemattoslupino@gmail.com>
2023-05-10 10:55:55 -0300

    Dashboard vs 1.0.0.3

--- js/script.js
+++ js/script.js
@@ -77,82 +77,82 @@ function showHide() {
 
 // --------------------------------------------------------------------------------
 
-function fazerid(){
+function fazerid() {
     let id = document.getElementById('id');
-    id.innerHTML = parseInt(Math.random()*1000000000);
+    id.innerHTML = parseInt(Math.random() * 1000000000);
 }
 
-function validardados(){
+function validardados() {
     let erro = document.getElementById('erro1');
     let nome = frm1.nome.value;
     let ende = frm1.ende.value;
     let cid = frm1.cid.value;
     let uf = frm1.uf.value;
 
-    if (nome.trim() == ''){
+    if (nome.trim() == '') {
         erro.innerHTML = "o campo nome não pode estar vazio!";
         erro.style.display = 'block';
         return false;
     }
-    if (nome.length < 3){
+    if (nome.length < 3) {
         erro.innerHTML = "o campo nome não pode ter menos de 3 caracteres!";
         erro.style.display = 'block';
         return false;
     }
-    if (nome.length > 100){
+    if (nome.length > 100) {
         erro.innerHTML = "o campo nome não pode ter mais de 100 caracteres!";
         erro.style.display = 'block';
         return false;
     }
     // 
-    if (ende.trim() == ''){
+    if (ende.trim() == '') {
         erro.innerHTML = "o campo endereço não pode estar vazio!";
         erro.style.display = 'block';
         return false;
     }
-    if (ende.length < 3){
+    if (ende.length < 3) {
         erro.innerHTML = "o campo endereço não pode ter menos de 3 caracteres!";
         erro.style.display = 'block';
         return false;
     }
-    if (ende.length > 100){
+    if (ende.length > 100) {
         erro.innerHTML = "o campo endereço não pode ter mais de 100 caracteres!";
         erro.style.display = 'block';
         return false;
     }
     // 
-    if (cid.trim() == ''){
+    if (cid.trim() == '') {
         erro.innerHTML = "o campo cidade não pode estar vazio!";
         erro.style.display = 'block';
         return false;
     }
-    if (cid.length < 3){
+    if (cid.length < 3) {
         erro.innerHTML = "o campo cidade não pode ter menos de 3 caracteres!";
         erro.style.display = 'block';
         return false;
     }
-    if (cid.length > 50){
+    if (cid.length > 50) {
         erro.innerHTML = "o campo cidade não pode ter mais de 50 caracteres!";
         erro.style.display = 'block';
         return false;
     }
     // 
-    else{
+    else {
         erro.style.display = 'none';
     }
     // 
-    if (uf.length >= 2){
+    if (uf.length >= 2) {
         let padrao = /^[A-Z]{2}$/
-        if (padrao.test(uf) == false){
+        if (padrao.test(uf) == false) {
             erro.innerHTML = "Coloque em maiuscula a unidade federativa!";
             erro.style.display = 'block';
             return false;
         }
-        else{
+        else {
             erro.style.display = 'none';
         }
     }
-    else{
+    else {
         erro.innerHTML = "Coloque uma unidade federativa válida!";
         erro.style.display = 'block';
         return false;
@@ -250,6 +250,7 @@ var valoresCertosCachorros9 = Math.round(aleatorioCachorros9);
 
 google.charts.load("current", { packages: ["corechart"] });
 google.charts.setOnLoadCallback(drawChart2);
+
 function drawChart2() {
     var data = google.visualization.arrayToDataTable([
         ["Element", "Principais raças de cachorros que cuidamos", { role: "style" }],
@@ -275,7 +276,7 @@ function drawChart2() {
         2]);
 
     var options = {
-        title: "Quais as principais raças de cachorros que nós tratamos, em média, em um mês",
+        title: "Quais as principais raças de cachorros que nós atendemos, em média, em um mês",
         width: 600,
         height: 400,
         bar: { groupWidth: "90%" },
@@ -288,4 +289,73 @@ function drawChart2() {
 };
 
 // --------------------------------------------------------------------------------
+
+// Gráfico 3
+
+var aleatorioEspecies = Math.random() * 20;
+var valoresCertosEspecies = Math.round(aleatorioEspecies);
+
+var aleatorioEspecies2 = Math.random() * 20;
+var valoresCertosEspecies2 = Math.round(aleatorioEspecies2);
+
+var aleatorioEspecies3 = Math.random() * 20;
+var valoresCertosEspecies3 = Math.round(aleatorioEspecies3);
+
+var aleatorioEspecies4 = Math.random() * 20;
+var valoresCertosEspecies4 = Math.round(aleatorioEspecies4);
+
+var aleatorioEspecies5 = Math.random() * 20;
+var valoresCertosEspecies5 = Math.round(aleatorioEspecies5);
+
+var aleatorioEspecies6 = Math.random() * 20;
+var valoresCertosEspecies6 = Math.round(aleatorioEspecies6);
+
+var aleatorioEspecies7 = Math.random() * 20;
+var valoresCertosEspecies7 = Math.round(aleatorioEspecies7);
+
+var aleatorioEspecies8 = Math.random() * 20;
+var valoresCertosEspecies8 = Math.round(aleatorioEspecies8);
+
+var aleatorioEspecies9 = Math.random() * 10;
+var valoresCertosEspecies9 = Math.round(aleatorioEspecies9);
+
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart3);
+
+function drawChart3() {
+    var data = google.visualization.arrayToDataTable([
+        ["Element", "Principais espécies de animais que cuidamos", { role: "style" }],
+        ["Calopsitas", valoresCertosEspecies, "#004c90"],
+        ["Equinos", valoresCertosEspecies2, "#116db5"],
+        ["Peixes pequenos", valoresCertosEspecies3, "#095da3"],
+        ["Hamsters", valoresCertosEspecies4, "#1a7ec8"],
+        ["Porquinhos-da-Índia", valoresCertosEspecies5, "#228eda"],
+        ["Ovinos", valoresCertosEspecies6, "#004c90"],
+        ["Suínos", valoresCertosEspecies7, "#116db5"],
+        ["Capivaras", valoresCertosEspecies8, "#095da3"],
+        ["Animais exóticos", valoresCertosEspecies9, "#1a7ec8"],
+    ]);
+
+    var view = new google.visualization.DataView(data);
+    view.setColumns([0, 1,
+        {
+            calc: "stringify",
+            sourceColumn: 1,
+            type: "string",
+            role: "annotation"
+        },
+        2]);
+
+    var options = {
+        title: "Quais as demais espécies de animais que nós tratamos, em média, em um mês",
+        width: 600,
+        height: 400,
+        bar: { groupWidth: "90%" },
+        legend: { position: "none" },
+        backgroundColor: "#4AF0B6",
+        hAxis: { gridlines: { color: 'black' } },
+    };
+    var chart = new google.visualization.BarChart(document.getElementById("Grafico3"));
+    chart.draw(view, options);
+};
 
